2c095e3d3fa40f2c727786c91406784fc421ecb5
kyung2 <lovelyhyunkyung@gmail.com>
2017-05-15 18:09:18 +0900

    0515comp

--- gearcon_m/Debug/.d
+++ gearcon_m/Debug/.d
@@ -1,6 +1,1 @@
-res/gearcon_m.edj: ../res/../edc_resource/eext_progressbar.edc \
-  ../res/../edc_resource/eext_spinner.edc ../res/../edc_resource/entry.edc \
-  ../res/../edc_resource/index.edc ../res/../edc_resource/image.edc \
-  ../res/../edc_resource/progressbar.edc ../res/../edc_resource/radio.edc \
-  ../res/../edc_resource/popup_custom.edc \
-  ../res/../edc_resource/scroller.edc ../res/../edc_resource/slider.edc
+res/gearcon_m.edj: ../res/../edc_resource/scroller.edc

b8d8a8859050842b9f0323d09e33a898d478ee72
kyung2 <lovelyhyunkyung@gmail.com>
2017-06-04 12:17:22 +0900

    수정

--- tizen/Gearcon/src/control_brightness.c
+++ tizen/Gearcon/src/control_brightness.c
@@ -97,13 +97,10 @@ void view_control_brightness(void *data)
 	layout = create_button_view(scroller);
 	elm_object_content_set(scroller, layout);
 
-	circle_scroller = eext_circle_object_scroller_add(scroller,
-			ad->circle_surface);
-	eext_circle_object_scroller_policy_set(circle_scroller,
-			ELM_SCROLLER_POLICY_OFF, ELM_SCROLLER_POLICY_AUTO);
+	circle_scroller = eext_circle_object_scroller_add(scroller,ad->circle_surface);
+	eext_circle_object_scroller_policy_set(circle_scroller,ELM_SCROLLER_POLICY_OFF, ELM_SCROLLER_POLICY_AUTO);
 
-	eext_rotary_object_event_callback_add(scroller,
-			_rotary_handler_brightness_cb, NULL);
+	eext_rotary_object_event_callback_add(scroller,_rotary_handler_brightness_cb, NULL);
 	eext_rotary_object_event_activated_set(scroller, EINA_TRUE);
 
 	nf_it = elm_naviframe_item_push(nf, "", NULL, NULL, scroller, NULL);
